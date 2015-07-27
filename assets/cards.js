5961a98499080a2cd541c3fff81a00e7d1be6a8e
zachausa <zachausa@gmail.com>
2015-07-26 17:52:17 -0700

    initial file drop

---
+++ assets/cards.js
@@ -0,0 +1,52 @@
+var parkDictionary = {};
+
+function fetchData() {
+
+	var rawTemplate = $('#thumbnail-template').html();
+
+  $.get('https://nationalpark.firebaseio.com/parks.json', function(parks) {
+   parkDictionary = parks;
+
+    for (var i = 0; i < parks.length; i++) {
+    	var currentPark = parks[i];
+    	var stampedTemplate = Mustache.render(rawTemplate, currentPark);
+    	$('#cards-container').append(stampedTemplate);
+    };
+
+    buildDictionary(parks);
+    bindEventListeners(); 
+  });
+}
+
+
+// when card click occurs
+function bindEventListeners() {
+$('.card').click(function(e){
+	var targetId= e.target.id;
+	var info = moviesDictionary[targetId];
+	// info.cast = info.cast.join(', ');
+	// console.log(info);
+	var rawTemplate = $('#lightbox-template').html();
+	// console.log(rawTemplate);
+	var stampedTemplate = Mustache.render(rawTemplate, info);	
+	
+	$('#lightbox-container').html(stampedTemplate);
+	$('#lightbox-container').fadeIn();
+	$('#mask').fadeIn();
+});
+
+	$('#mask').click(function(){
+	  $('#lightbox-container').fadeOut();
+	  $('#mask').fadeOut();
+});
+}	
+
+function buildDictionary(parks) {
+  for (var i = 0; i < parks.length; i++) {
+    var currentPark = parks[i];
+    parkDictionary[currentPark.id] = currentPark;
+  };
+  
+}
+
+fetchData();
